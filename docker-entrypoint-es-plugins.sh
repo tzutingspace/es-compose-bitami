83723f3962caf83a9916d50af7060aeaaa136abe
tzutingspace <tzutingh2@gmail.com>
2023-10-13 14:20:56 +0800

    modify

--- docker-entrypoint-es-plugins.sh
+++ docker-entrypoint-es-plugins.sh
@@ -3,10 +3,12 @@
 # 定義插件路徑
 PLUGIN_DIR="/opt/bitnami/elasticsearch/plugins"
 
+echo "Starting plugin installation..."
+
 # 檢查並安裝 analysis-icu
-if [ ! -d "$PLUGIN_DIR/analysis-icu" ]; then
-  /opt/bitnami/elasticsearch/bin/elasticsearch-plugin install analysis-icu
-fi
+# if [ ! -d "$PLUGIN_DIR/analysis-icu" ]; then
+#   /opt/bitnami/elasticsearch/bin/elasticsearch-plugin install analysis-icu
+# fi
 
 # 檢查並安裝 analysis-kuromoji
 if [ ! -d "$PLUGIN_DIR/analysis-kuromoji" ]; then
@@ -23,10 +25,13 @@ if [ ! -d "$PLUGIN_DIR/analysis-smartcn" ]; then
   /opt/bitnami/elasticsearch/bin/elasticsearch-plugin install analysis-smartcn
 fi
 
-# 檢查並安裝 ingest-langdetect
-if [ ! -d "$PLUGIN_DIR/analysis-ik" ]; then
-  /opt/bitnami/elasticsearch/bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.4.2/elasticsearch-analysis-ik-7.4.2.zip
-fi
+# # 檢查並安裝 ingest-langdetect
+# if [ ! -d "$PLUGIN_DIR/analysis-ik" ]; then
+#   /opt/bitnami/elasticsearch/bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.4.2/elasticsearch-analysis-ik-7.4.2.zip
+# fi
+
+
+echo "Plugin installation completed."
 
 # # 啟動 Elasticsearch
 # exec /entrypoint.sh /run.sh
